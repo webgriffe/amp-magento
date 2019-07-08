1d1833debdeeed8ab045b2bfed086a7a2002c29c
Andrea Zambon <azambon@webgriffe.com>
2019-07-08 14:49:45 +0200

    Expands the test code to handle complex conditions

--- src/InMemoryMagento/Utils.php
+++ src/InMemoryMagento/Utils.php
@@ -44,8 +44,8 @@ trait Utils
         if (!array_key_exists('searchCriteria', $parsedQuery)) {
             throw new \Error('Parameter searchCriteria is required');
         }
-        $filter = $parsedQuery['searchCriteria']['filterGroups'][0]['filters'][0] ?? null;
-        if (!$filter) {
+
+        if (!$parsedQuery['searchCriteria'] || !$parsedQuery['searchCriteria']['filterGroups']) {
             return new ResponseStub(
                 200,
                 json_encode(
@@ -57,52 +57,65 @@ trait Utils
                 )
             );
         }
-        $data = array_filter($data, function (\stdClass $element) use ($filter) {
-            $field = $filter['field'];
-            $actualValue = $element->$field ?? null;
-            if (null === $actualValue) {
-                if (property_exists($element, 'custom_attributes') && $element->custom_attributes) {
-                    $customAttributes = $element->custom_attributes;
-                    $actualValue = array_reduce($customAttributes, function ($carry, $customAttribute) use ($field) {
-                        if ($customAttribute->attribute_code === $field) {
-                            return $customAttribute->value;
+
+        foreach ($parsedQuery['searchCriteria']['filterGroups'] as $filterGroup) {
+            $groupData = [];
+            foreach ($filterGroup['filters'] as $filter) {
+                //Filters are in OR between each other
+                $singleFilterData = array_filter($data, function (\stdClass $element) use ($filter) {
+                    $field = $filter['field'];
+                    $actualValue = $element->$field ?? null;
+                    if (null === $actualValue) {
+                        if (property_exists($element, 'custom_attributes') && $element->custom_attributes) {
+                            $customAttributes = $element->custom_attributes;
+                            $actualValue = array_reduce($customAttributes, function ($carry, $customAttribute) use ($field) {
+                                if ($customAttribute->attribute_code === $field) {
+                                    return $customAttribute->value;
+                                }
+                            });
                         }
-                    });
-                }
+                    }
+
+                    if (empty($filter['conditionType'])) {
+                        $filter['conditionType'] = 'eq';
+                    }
+
+                    switch ($filter['conditionType']) {
+                        case 'eq':
+                            return $actualValue === $filter['value'];
+                        case 'neq':
+                            return $actualValue !== $filter['value'];
+                        case 'gt':
+                            return $actualValue > $filter['value'];
+                        case 'gteq':
+                            return $actualValue >= $filter['value'];
+                        case 'lt':
+                            return $actualValue < $filter['value'];
+                        case 'lteq':
+                            return $actualValue <= $filter['value'];
+                        case 'in':
+                            return in_array($actualValue, explode(',', $filter['value']));
+                        case 'nin':
+                            return !in_array($actualValue, explode(',', $filter['value']));
+                        case 'null':
+                            return is_null($actualValue);
+                        case 'notnull':
+                            return !is_null($actualValue);
+                        case 'like':
+                            $regex = str_replace('%', '.*', '/'.preg_quote($filter['value'], '/').'/');
+                            return preg_match($regex, $actualValue);
+                        default:
+                            throw new \Error(sprintf('Condition Type "%s" not supported', $filter['conditionType']));
+                    }
+                });
+
+                //+ operator preserves numeric keys, so duplicate values should not happen
+                $groupData += $singleFilterData;
             }
 
-            if (empty($filter['conditionType'])) {
-                $filter['conditionType'] = 'eq';
-            }
-
-            switch ($filter['conditionType']) {
-                case 'eq':
-                    return $actualValue === $filter['value'];
-                case 'neq':
-                    return $actualValue !== $filter['value'];
-                case 'gt':
-                    return $actualValue > $filter['value'];
-                case 'gteq':
-                    return $actualValue >= $filter['value'];
-                case 'lt':
-                    return $actualValue < $filter['value'];
-                case 'lteq':
-                    return $actualValue <= $filter['value'];
-                case 'in':
-                    return in_array($actualValue, explode(',', $filter['value']));
-                case 'nin':
-                    return !in_array($actualValue, explode(',', $filter['value']));
-                case 'null':
-                    return is_null($actualValue);
-                case 'notnull':
-                    return !is_null($actualValue);
-                case 'like':
-                    $regex = str_replace('%', '.*', '/'.preg_quote($filter['value'], '/').'/');
-                    return preg_match($regex, $actualValue);
-                default:
-                    throw new \Error(sprintf('Condition Type "%s" not supported', $filter['conditionType']));
-            }
-        });
+            $data = $groupData;
+        }
+        
         return new ResponseStub(
             200,
             json_encode(
