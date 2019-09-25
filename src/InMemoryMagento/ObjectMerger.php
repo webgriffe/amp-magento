3ba8cee65967d1992ecac41e55b12411bb6a462a
Andrea Zambon <azambon@webgriffe.com>
2019-09-25 10:36:07 +0200

    Improves the object merger code

--- src/InMemoryMagento/ObjectMerger.php
+++ src/InMemoryMagento/ObjectMerger.php
@@ -17,11 +17,51 @@ final class ObjectMerger
      */
     public static function merge(\stdClass $product1, \stdClass $product2): \stdClass
     {
-        $result = clone $product1;
-        foreach (array_keys(get_object_vars($product2)) as $fieldName) {
-            $result->{$fieldName} = $product2->{$fieldName};
+        return self::mergeGeneric($product1, $product2);
+    }
+
+    private static function mergeGeneric($elem1, $elem2)
+    {
+        if ($elem1 instanceof \stdClass) {
+            $result = clone $elem1;
+        } elseif (is_array($elem1)) {
+            $result = $elem1;
+        } else {
+            return $elem2;
         }
+
+        if ($elem2 instanceof \stdClass) {
+            foreach (array_keys(get_object_vars($elem2)) as $fieldName) {
+                self::mergeValue($result, $fieldName, $elem2->{$fieldName});
+            }
+        } elseif (is_array($elem2)) {
+            foreach ($elem2 as $key => $value) {
+                self::mergeValue($result, $key, $value);
+            }
+        } else {
+            //Non-object and non-array values. Can't merge, and in this case we must give precedence to the second
+            //element
+            return $elem2;
+        }
+
         return $result;
+    }
+
+    private static function mergeValue(&$mergeInto, $fieldName, $value)
+    {
+        if ($mergeInto instanceof \stdClass) {
+            if (isset($mergeInto->{$fieldName})) {
+                $value = self::mergeGeneric($mergeInto->{$fieldName}, $value);
+            }
+            $mergeInto->{$fieldName} = $value;
+        } elseif (is_array($mergeInto)) {
+            if (array_key_exists($fieldName, $mergeInto)) {
+                $value = self::mergeGeneric($mergeInto[$fieldName], $value);
+            }
+            $mergeInto[$fieldName] = $value;
+        } else {
+            throw new \Exception("Cannot set a value into a field of a scalar value");
+        }
     }
 
     private static function objectToArray(\stdClass $object): array
